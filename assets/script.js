150eab92268ba498fe2cbd9341c77d1cb74f0575
MrCybernetic <florian.lobert@fr.abb.com>
2023-02-11 15:20:47 +0100

    website page

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+document.getElementById("defaultOpen").click();
+
+function openGSDMLtab(evt, GSDMLType) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(GSDMLType).style.display = "block";
+    evt.currentTarget.className += " active";
+}
